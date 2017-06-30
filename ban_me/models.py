7f2c1eb6e53bf71030052d00506995a719060ed4
eskimopies <eskimopies1999@gmail.com>
2017-06-30 11:14:12 +0100

    Attempting the long-term storage solution

--- ban_me/models.py
+++ ban_me/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    """A saved post from Reddit"""
+    title = models.TextField()
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.title
