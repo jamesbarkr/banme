bd9f473c9c9ad05c4b422ce6cb9b2e670c768c37
eskimopies <eskimopies1999@gmail.com>
2017-06-30 15:37:06 +0100

    Cleaned up code

--- ban_me/markovit.py
+++ ban_me/markovit.py
@@ -6,7 +6,7 @@ from . import sample
 
 # sampletext = sample.sample() # for offline testing
 
-def markovit(textlist): # NOW REDUNDANT
+def markovit(textlist): # NOW REDUNDANT + HORRIBLY MESSY, KEEP COLLAPSE
     """
     A function which turns an input list (strings) into a Markov'd
     output
@@ -46,31 +46,39 @@ def markovit(textlist): # NOW REDUNDANT
 
 def wordlistgen(titlelist):
     """Generates a a dictionary of words, each with a dictionary of potential following words"""
-    return_len = 3
     word_freq = {}
     for title in titlelist:
         words = title.split() # splits string into words
+
         for word in words:
             if word not in word_freq.keys(): # if a list value does not exist in the
                 word_freq[word] = []         # dict yet, create one
-        i = 0
-        while i < len(words): # super janky js-style loop TODO beautify this
-            currentWord = words[i]
-            print(currentWord)
-            if i < (len(words) - 1):
-                nextWord = words[i + 1]
+
+        for num in range(len(words)): # a for loop which populates the lists in the dictionary
+            currentWord = words[num]
+            if num < (len(words) - 1):
+                nextWord = words[num + 1]
                 word_freq[currentWord].append(nextWord)
-            i += 1
+
+    return word_freq
+
+def markovit_v2(titlelist):
+    word_freq = wordlistgen(titlelist) #
+    return_len = 10 # the length of the returned string
 
     startSource = titlelist[random.randrange(len(titlelist))]
-    currentWord = startSource.split()[0]
-    result = currentWord
+    currentWord = startSource.split()[0] # selects a 'first word' from among the posts
+    result = '' # initializes the result sentence
 
-    j = 0
-    while j < return_len - 1:
-        j += 1
-        possibilities = word_freq[currentWord]
-        nextWord = possibilities[random.randrange(len(possibilities))]
+    for j in range(return_len):
+        possibilities = word_freq[currentWord] # gets all possible next words
+        if len(possibilities) != 0: # if there is at least one possibility, carry on.
+            nextWord = possibilities[random.randrange(len(possibilities))]
+        else: # this entire part of the loop should be REMOVED <-- TODO its for making sure its not broken
+            possibilities = []
+            for word in word_freq.keys():
+                possibilities.append(word)
+            nextWord = possibilities[random.randrange(len(word_freq.keys()))]
         result += ' ' + nextWord
         currentWord = nextWord
 
