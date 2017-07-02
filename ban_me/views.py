93277eb7aca2aa2f166ef4400c1b244fc1553054
ryanjiang <ryan.qiyu.jiang@gmail.com>
2017-07-01 23:48:04 -0400

    angular integration attempt 1

--- ban_me/views.py
+++ ban_me/views.py
@@ -9,28 +9,28 @@ from . import markovit
 def main(request):
     """Main page for ban_me"""
     # get data and return a status code (200 = good, 429 = bad)
-    url = 'https://www.reddit.com/r/marchagainsttrump/top.json'
-    r = requests.get(url)
-    print("Status code: ", r.status_code)
-    if r.status_code == 200:
-        print("Code good.")
-    else:
-        print("Bad status.")
+    #url = 'https://www.reddit.com/r/marchagainsttrump/top.json'
+    #r = requests.get(url)
+    #print("Status code: ", r.status_code)
+    #if r.status_code == 200:
+    #    print("Code good.")
+    #else:
+    #    print("Bad status.")
 
     # store API response in a var
-    response_dict = r.json()
+   # response_dict = r.json()
 
     # process results
-    post_titles = []
+    #post_titles = []
 
     # turn the json data into a list of strings (titles)
-    for post in response_dict['data']['children']:
-        post_titles.append(post['data']['title'])
+    #for post in response_dict['data']['children']:
+    #    post_titles.append(post['data']['title'])
 
     # run the post data through markovit
-    output = markovit.markovit_v2(post_titles)
+    #output = markovit.markovit_v2(post_titles)
 
     # context is info passed to the template using render
-    context = {'titles': post_titles, 'output': output}
+    #context = {'titles': post_titles, 'output': output}
 
-    return render(request, 'ban_me/main.html', context)
+    return render(request, 'views/index.html')
