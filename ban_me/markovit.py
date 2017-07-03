266e008e7bd2f27703577dd9a90ac39648717b28
eskimopies <eskimopies1999@gmail.com>
2017-07-03 16:24:55 +0100

    Made markov chains work with models
    - Removed a lot of superfluous code

--- ban_me/markovit.py
+++ ban_me/markovit.py
@@ -1,48 +1,7 @@
 """The code for building a Markov chain output"""
 
 import random
-import re
-from . import sample
-
-# sampletext = sample.sample() # for offline testing
-
-def markovit(textlist): # NOW REDUNDANT + HORRIBLY MESSY, KEEP COLLAPSE
-    """
-    A function which turns an input list (strings) into a Markov'd
-    output
-    """
-    # the n part of the ngram
-    order = 6 # increase/decrease this number to have the output make more/less sense, respectively
-    # a dictionary to keep the ngrams in
-    ngrams = {}
-    # a function which iterates through the list of posts
-    for text in textlist:
-        i = 0
-        while i < ((len(text) - order) - 1): # convoluted code to avoid out-of-range errors
-            # sets the current gram equal to a spliced bit of text as
-            # as long as the order
-            gram = text[i:(i + order)]
-            # if the gram isn't in the dictionary yet, make a key and
-            # set an empty list as the value
-            if gram not in ngrams.keys():
-                ngrams[gram] = []
-            # adds the letter following the gram to the list in the dict
-            ngrams[gram].append(text[i + order])
-            i += 1
-
-    currentGram = textlist[random.randrange(len(textlist))][0:order] # picks a starting gram from a random title
-    result = currentGram
-
-    j = 0 # TODO find a way to make this prettier
-    while j < 100: # <-- increase this number for longer string output
-        j += 1
-        if currentGram in ngrams: # fixed recurring KeyError
-            possibilities = ngrams[currentGram] # the potential next letters
-            nextletter = possibilities[random.randrange(len(possibilities))] # TODO: remove redundancy
-            result += nextletter # appends the next letter to the result
-            currentGram = result[(len(result) - order):len(result)] # makes a new currentGram from the appended result
-
-    return result
+from .models import Post
 
 def wordlistgen(titlelist):
     """Generates a a dictionary of words, each with a dictionary of potential following words"""
@@ -62,7 +21,11 @@ def wordlistgen(titlelist):
 
     return word_freq
 
-def markovit_v2(titlelist):
+def markovit_v2():
+    titlelist = []
+    posts = Post.objects.all()
+    for post in posts:
+        titlelist.append(post.title)
     word_freq = wordlistgen(titlelist) #
     return_len = 10 # the length of the returned string
 
--- ban_me/views.py
+++ ban_me/views.py
@@ -1,41 +1,14 @@
 from django.shortcuts import render
 
 import requests
-import json
 
 from . import markovit
 
-# Create your views here.
-
 def main(request):
     """Main page for ban_me"""
-    # get data and return a status code (200 = good, 429 = bad)
-    url = 'https://www.reddit.com/r/marchagainsttrump/top.json'
-    headers = {'User-agent': 'u/eskimopies webapp ban_me, alpha testing using Django, requests'}
-    r = requests.get(url, headers)
-    print("Status code: ", r.status_code)
-    if r.status_code == 200:
-        print("Code good.")
-    else:
-        print("Bad status.")
-
-    # store API response in a var
-    response_dict = json.loads(r.text)
-    print(response_dict)
-
-    # process results
-    post_titles = []
-
-    response = response_dict['data']
-
-    # turn the json data into a list of strings (titles)
-    for post in response['children']:
-        post_titles.append(post['data']['title'])
-
-    # run the post data through markovit
-    output = markovit.markovit_v2(post_titles)
+    output = markovit.markovit_v2()
 
     # context is info passed to the template using render
-    context = {'titles': post_titles, 'output': output}
+    context = {'output': output}
 
     return render(request, 'ban_me/main.html', context)
